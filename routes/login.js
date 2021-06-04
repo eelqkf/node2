fa9b6f9581ad095625f84a561769a938d164b1e8
eelqkf <84161235+eelqkf@users.noreply.github.com>
2021-06-05 00:08:29 +0900

    Add files via upload

---
+++ routes/login.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middleware');
+const post = require('../models/post');
+
+const router = express.Router();
+
+
+router.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
+router.get('/main', isLoggedIn, (req,res)=>{
+res.render('main')
+});
+// router.get('/main/qna', isLoggedIn, (req,res)=>{
+//   res.render('qna')
+//   console.log(user.data);
+//   });
+
+router.get('/join', isNotLoggedIn, (req, res) => {
+  res.render('join');
+});
+
+router.get('/', (req, res, next) => {
+  const twits = [];
+  res.render('login', {
+    twits,
+  });
+});
+
+
+module.exports = router;
